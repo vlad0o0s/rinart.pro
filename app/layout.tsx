f9342d6188700939b42e0968fc3847dbf8bf63e8
vlad0o0s <vlad-2028@mail.ru>
2025-11-14 14:56:26 +0300

    feat: add yandex metrika

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { headers } from "next/headers";
 import localFont from "next/font/local";
+import Script from "next/script";
 import "./globals.css";
 import { PageTransition } from "@/components/page-transition";
 import { JsonLd } from "@/components/json-ld";
@@ -71,6 +72,31 @@ export default async function RootLayout({
   return (
     <html lang="ru" data-scroll-behavior="smooth">
       <body className={`${neueHaasUnica.variable} antialiased`}>
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) {
+                  return;
+                }
+              }
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105324096', 'ym');
+
+            ym(105324096, 'init', { ssr: true, webvisor: true, clickmap: true, ecommerce: "dataLayer", accurateTrackBounce: true, trackLinks: true });
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/105324096"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
         <div className="site-shell">
           <PageTransition enabled={!isBot} />
           <RouteReadyAnnouncer />
